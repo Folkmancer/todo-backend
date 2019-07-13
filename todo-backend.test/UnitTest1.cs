b57675a7310c04bcbf04df3ab8fbbb1e2fbbfa9f
folkmancer <folkmancer@gmail.com>
2019-07-13 13:31:59 +0500

    Добавлен тест

---
+++ todo-backend.test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace todo_backend.test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
