ff8fc610249938cf1cc154122e4cb12eb18158d9
folkmancer <folkmancer@gmail.com>
2019-07-13 13:42:39 +0500

    Переименованно пространство имён api

--- todo-backend/Startup.cs
+++ todo-backend/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace todo_backend
+namespace todobackend
 {
     public class Startup
     {
