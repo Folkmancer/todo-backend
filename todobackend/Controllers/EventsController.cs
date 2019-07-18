a36bff92b949962ff1545a93e55deb7a5abbb5e5
Folkmancer <folkmancer@gmail.com>
2019-07-18 19:52:25 +0500

    Delete default template controller and create new controller for events

---
+++ todobackend/Controllers/EventsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace todobackend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        // GET: api/events
+        [HttpGet]
+        public async Task<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/events/{id}
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/events
+        [HttpPost]
+        public async Task Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/events/{id}
+        [HttpPut("{id}")]
+        public async Task Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/events/{id}
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+        }
+    }
+}
