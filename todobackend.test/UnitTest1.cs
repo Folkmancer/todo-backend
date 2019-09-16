97412295e2af165fefbf3a71004be2e8bde1b49b
Folkmancer <folkmancer@gmail.com>
2019-09-16 13:16:04 +0300

    New test for POST

--- todobackend.test/UnitTest1.cs
+++ todobackend.test/UnitTest1.cs
@@ -1,33 +1,20 @@
-using todobackend.Models;
-using todobackend.Controllers;
-using todobackend;
-using Moq;
-using System.Linq;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using Xunit;
+using System.Linq;
+using System.Threading.Tasks;
+using todobackend.Controllers;
+using todobackend.Models;
 
 namespace todobackend.test
 {
-
+    [TestClass]
     public class EventsControllerUnitTest
     {
-        public void Seed(DataBaseContext context)
+        private IQueryable<Event> GetTestEvents()
         {
-            context.Database.EnsureDeleted();
-            context.Database.EnsureCreated();
-            context.Events.AddRange(GetTestEvents());
-            context.SaveChanges();
-        }
-
-        private IEnumerable<Event> GetTestEvents()
-        {
-            var events = new List<Event>();
-            var temp = new Event[] {
+            return new Event[] {
                     new Event { Id = 1, Description = "Test1", DeadlineDate = System.DateTimeOffset.Parse("12.06.2019"), IsComplete = false },
                     new Event { Id = 2, Description = "Test2", DeadlineDate = System.DateTimeOffset.Parse("13.02.2019"), IsComplete = false },
                     new Event { Id = 3, Description = "Test3", DeadlineDate = System.DateTimeOffset.Parse("14.06.2019"), IsComplete = false },
@@ -43,41 +30,28 @@ namespace todobackend.test
                     new Event { Id = 13, Description = "Test13", DeadlineDate = System.DateTimeOffset.Parse("16.06.2019"), IsComplete = false },
                     new Event { Id = 14, Description = "Test14", DeadlineDate = System.DateTimeOffset.Parse("15.06.2019"), IsComplete = false },
                     new Event { Id = 15, Description = "Test15", DeadlineDate = System.DateTimeOffset.Parse("4.12.2019"), IsComplete = false }
-                };
-            events.AddRange(temp);
-            return events;
+                }.AsQueryable();
         }
 
-        [Fact]
-        public void TestMethod1()
+        [TestMethod]
+        public async Task TestMethod1()
         {
-            // Arrange
-            var data = GetTestEvents().AsQueryable();
-            var mockSet = new Mock<DbSet<Event>>();
-            mockSet.As<IQueryable<Event>>().Setup(m => m.Provider).Returns(data.Provider);
-            mockSet.As<IQueryable<Event>>().Setup(m => m.Expression).Returns(data.Expression);
-            mockSet.As<IQueryable<Event>>().Setup(m => m.ElementType).Returns(data.ElementType);
-            mockSet.As<IQueryable<Event>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
-            var mockContext = new Mock<DataBaseContext>();
-            mockContext.Setup(c => c.Events).Returns(mockSet.Object);
-            var controller = new EventsController(mockContext.Object);
+            var options = new DbContextOptionsBuilder<DataBaseContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
 
-            // Act
-            var result = controller.GetAllEvents();
+            using (var context = new DataBaseContext(options))
+            {
+                var service = new EventsController(context);
+                await service.Create(new EventProxy(GetTestEvents().First()), new ApiVersion(1, 0));
+                context.SaveChanges();
+            }
 
-            // Assert
-            Assert.IsType<OkObjectResult>(result);
-            /*
-            var mockSet = new Mock<DbSet<Event>>();
-
-            var mockContext = new Mock<DataBaseContext>();
-            mockContext.Setup(m => m.Events).Returns(mockSet.Object);
-
-            var service = new BlogService(mockContext.Object);
-            service.AddBlog("ADO.NET Blog", "http://blogs.msdn.com/adonet");
-
-            mockSet.Verify(m => m.Add(It.IsAny<Blog>()), Times.Once());
-            mockContext.Verify(m => m.SaveChanges(), Times.Once());*/
+            using (var context = new DataBaseContext(options))
+            {
+                Assert.AreEqual(1, context.Events.Count());
+                Assert.AreEqual(GetTestEvents().First().Id, context.Events.First().Id);
+            }
         }
     }
 }
