3740bcc1a9c1c3e954732bf69659de6b781c006c
Folkmancer <folkmancer@gmail.com>
2019-09-19 19:18:42 +0300

    Changed communication logic between Event and NewEvent

--- backend/Models/NewEvent.cs
+++ backend/Models/NewEvent.cs
@@ -10,5 +10,13 @@ namespace backend.Models
         public string Description { get; set; }
         public long? DeadlineDate { get; set; }
         public bool IsComplete { get; set; }
+
+        public Event ToEvent()
+        {
+            var description = this.Description;
+            var deadlineDate = (this.DeadlineDate.HasValue) ? DateTimeOffset.FromUnixTimeSeconds(this.DeadlineDate.Value) : default;
+            var isComplete = this.IsComplete;
+            return new Event { Description = description, DeadlineDate = deadlineDate, IsComplete = isComplete };
+        }
     }
 }
