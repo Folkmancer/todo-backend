5314ad7bb865ceae3a4379131f21ad2f1e61cc4f
Folkmancer <folkmancer@gmail.com>
2019-09-19 12:40:27 +0300

    Added new class NewEvent for post and put methode

---
+++ backend/Models/NewEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    public class NewEvent
+    {
+        public string Description { get; set; }
+        public long? DeadlineDate { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
