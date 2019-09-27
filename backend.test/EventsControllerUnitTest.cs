06cf92f911fea0ad77a911edb423e57039872048
Folkmancer <folkmancer@gmail.com>
2019-09-27 13:01:45 +0300

    Refactor fix

--- backend.test/EventsControllerUnitTest.cs
+++ backend.test/EventsControllerUnitTest.cs
@@ -1,6 +1,7 @@
 using backend.Controllers;
 using backend.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
@@ -11,7 +12,96 @@ namespace backend.test
 {
     [TestClass]
     public class EventsControllerUnitTest
-    {  
+    {
+        [TestMethod]
+        public async Task GetAllEvents_Ok_Result()
+        {
+            var options = new DbContextOptionsBuilder<DataBaseContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            var context = new DataBaseContext(options);
+            var service = new EventsController(context);
+            var eventsTest = new[] {
+                new Event {
+                    Id = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6"), Description = "Test1",
+                    DeadlineDate = DateTimeOffset.FromUnixTimeSeconds(1560286800), IsComplete = false
+                },
+                new Event {
+                    Id = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa7"), Description = "Test2",
+                    DeadlineDate = DateTimeOffset.FromUnixTimeSeconds(1560286800), IsComplete = false
+                },
+                new Event {
+                    Id = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa8"), Description = "Test3",
+                    DeadlineDate = DateTimeOffset.FromUnixTimeSeconds(1560286800), IsComplete = false
+                }
+            };
+            context.Events.AddRange(eventsTest);
+            context.SaveChanges();
+
+            var result = await service.GetAllEvents();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, context.Events.Count());
+        }
+
+        [TestMethod]
+        public async Task GetAllEvents_NotFound_Result()
+        {
+            var options = new DbContextOptionsBuilder<DataBaseContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            var context = new DataBaseContext(options);
+            var service = new EventsController(context);
+
+            var result = await service.GetAllEvents();
+            var notFoundResult = result.Result as NotFoundResult;
+
+            Assert.IsNotNull(notFoundResult);
+            Assert.AreEqual(404, notFoundResult.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task GetById_Ok_Result()
+        {
+            var options = new DbContextOptionsBuilder<DataBaseContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            var context = new DataBaseContext(options);
+            var service = new EventsController(context);
+            var id = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6");
+            var eventTest = new Event
+            {
+                Id = id,
+                Description = "Test1",
+                DeadlineDate = DateTimeOffset.FromUnixTimeSeconds(1560286800),
+                IsComplete = false
+            };
+            
+            context.Events.Add(eventTest);
+            context.SaveChanges();
+
+            var result = await service.GetById(id);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(id, result.Value.Id);
+        }
+
+        [TestMethod]
+        public async Task GetById_NotFound_Result()
+        {
+            var options = new DbContextOptionsBuilder<DataBaseContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            var context = new DataBaseContext(options);
+            var service = new EventsController(context);
+
+            var result = await service.GetById(Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6"));
+            var notFoundResult = result.Result as NotFoundResult;
+
+            Assert.IsNotNull(notFoundResult);
+            Assert.AreEqual(404, notFoundResult.StatusCode);
+        }
+
         [TestMethod]
         public async Task TestPostMethod()
         {
@@ -19,18 +109,82 @@ namespace backend.test
                 .UseInMemoryDatabase(Guid.NewGuid().ToString())
                 .Options;
             var eventTest = new NewEvent { Description = "Test1", DeadlineDate = 1560286800, IsComplete = false };
+            var context = new DataBaseContext(options);
+            var service = new EventsController(context);
 
-            using (var context = new DataBaseContext(options))
-            {
-                var service = new EventsController(context);
-                await service.Create(eventTest, new ApiVersion(1, 0));
-                context.SaveChanges();
-            }
+            var result = await service.Create(eventTest, new ApiVersion(1, 0));
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, context.Events.Count());
+        }
 
-            using (var context = new DataBaseContext(options))
-            {
-                Assert.AreEqual(1, context.Events.Count());
-            }
+        [TestMethod]
+        public async Task UpdateById_Ok_Result()
+        {
+            var options = new DbContextOptionsBuilder<DataBaseContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            var eventTest = new Event { Id = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6"), Description = "Test1", DeadlineDate = DateTimeOffset.FromUnixTimeSeconds(1560286800), IsComplete = false };
+            var updateEventTest = new UpdateEvent { Description = "UpdateTest1", DeadlineDate = 1560286800, IsComplete = false };
+            var context = new DataBaseContext(options);
+            var service = new EventsController(context);
+            context.Events.Add(eventTest);
+
+            var result = await service.UpdateById(Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6"), updateEventTest);
+            var okResult = result as OkResult;
+
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task UpdateById_NotFound_Result()
+        {
+            var options = new DbContextOptionsBuilder<DataBaseContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options; ;
+            var updateEventTest = new UpdateEvent { Description = "UpdateTest1", DeadlineDate = 1560286800, IsComplete = false };
+            var context = new DataBaseContext(options);
+            var service = new EventsController(context);
+
+            var result = await service.UpdateById(Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6"), updateEventTest);
+            var notFoundResult = result as NotFoundResult;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(404, notFoundResult.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task DeleteById_Ok_Result()
+        {
+            var options = new DbContextOptionsBuilder<DataBaseContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            var eventTest = new Event { Id = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6"), Description = "Test1", DeadlineDate = DateTimeOffset.FromUnixTimeSeconds(1560286800), IsComplete = false };
+            var context = new DataBaseContext(options);
+            var service = new EventsController(context);
+            context.Events.Add(eventTest);
+
+            var result = await service.DeleteById(Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6"));
+            var okResult = result as OkResult;
+
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task DeleteById_NotFound_Result()
+        {
+            var options = new DbContextOptionsBuilder<DataBaseContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;;
+            var context = new DataBaseContext(options);
+            var service = new EventsController(context);
+
+            var result = await service.DeleteById(Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6"));
+            var notFoundResult = result as NotFoundResult;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(404, notFoundResult.StatusCode);
         }
     }
 }
---
+++ backend/Models/UpdateEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace backend.Models
+{
+    public class UpdateEvent
+    {
+        public string Description { get; set; }
+        public long? DeadlineDate { get; set; }
+        public bool IsComplete { get; set; }
+
+        public void Fill(Event e)
+        {
+            e.Description = this.Description;
+            e.DeadlineDate = (this.DeadlineDate.HasValue) ? DateTimeOffset.FromUnixTimeSeconds(this.DeadlineDate.Value) : default;
+            e.IsComplete = this.IsComplete;
+        }
+    }
+}
